58c195e10a42a9321674dff5c9f3b94d850b76d2
Yusun Choi <nsuy.ch@gmail.com>
2024-03-21 04:56:30 +0900

    chore: Add initial application

---
+++ src/main/java/com/naeun/naeun_server/NaeunServerApplication.java
@@ -0,0 +1,13 @@
+package com.naeun.naeun_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaeunServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NaeunServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naeun/naeun_server/NaeunServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naeun.naeun_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaeunServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
