e051fbca38b6d98e385541972a0c77f0c0e31e02
Yusun Choi <nsuy.ch@gmail.com>
2024-03-24 02:07:45 +0900

    feat: Enable JPA auditing & set server time-zone (#1)

--- src/main/java/com/naeun/naeun_server/NaeunServerApplication.java
+++ src/main/java/com/naeun/naeun_server/NaeunServerApplication.java
@@ -1,10 +1,19 @@
 package com.naeun.naeun_server;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NaeunServerApplication {
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(NaeunServerApplication.class, args);
