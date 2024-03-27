2f29f8133c851df7318ac521a1b532560943fd44
Yusun Choi <nsuy.ch@gmail.com>
2024-03-28 00:43:06 +0900

    test: Add token refresh API test (#3)

--- src/test/java/com/naeun/naeun_server/domain/User/controller/UserControllerTest.java
+++ src/test/java/com/naeun/naeun_server/domain/User/controller/UserControllerTest.java
@@ -2,8 +2,10 @@ package com.naeun.naeun_server.domain.User.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.naeun.naeun_server.domain.User.dto.LoginReqDto;
-import com.naeun.naeun_server.global.util.RedisUtil;
+import com.naeun.naeun_server.domain.User.dto.LoginResDto;
+import com.naeun.naeun_server.domain.User.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -29,11 +32,12 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private RedisUtil redisUtil;
+    private UserService userService;
 
     private final String API_URL = "/api/v1/user";
 
     @Test
+    @DisplayName("새로운 유저 로그인 성공")
     void login() throws Exception {
         // given
         String id = "test-45test";
@@ -43,6 +47,25 @@ class UserControllerTest {
         ResultActions resultActions = mockMvc.perform(post(API_URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsString(dto)));
+
+        // then
+        resultActions.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data.userId").exists())
+                .andExpect(jsonPath("$.data.accessToken").exists())
+                .andExpect(jsonPath("$.data.refreshToken").exists())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("토큰 재발급 성공")
+    void refreshTokens() throws Exception {
+        // given
+        LoginResDto dto = userService.login(new LoginReqDto("test-45test"));
+        String refreshToken = dto.getRefreshToken();
+
+        // when
+        ResultActions resultActions = mockMvc.perform(get(API_URL + "/token")
+                .header("Authorization-refresh", "Bearer " + refreshToken));
 
         // then
         resultActions.andExpect(status().isCreated())
