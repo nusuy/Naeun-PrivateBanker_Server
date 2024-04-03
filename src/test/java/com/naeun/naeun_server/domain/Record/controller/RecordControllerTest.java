f926913b8f9ab22fc42cf2f6d0bd5508368600ff
Yusun Choi <nsuy.ch@gmail.com>
2024-04-03 19:34:42 +0900

    test: Add record detail & new record API test (#8)

--- src/test/java/com/naeun/naeun_server/domain/Record/controller/RecordControllerTest.java
+++ src/test/java/com/naeun/naeun_server/domain/Record/controller/RecordControllerTest.java
@@ -14,16 +14,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.FileInputStream;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -89,6 +93,39 @@ class RecordControllerTest {
         resultActions
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data[0].title").value("test-data 2"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("녹취 데이터 추가 성공")
+    void addNewRecord() throws Exception {
+        // given
+        String fileName = "test";
+        String fileType = "audio/m4a";
+        String filePath = "src/test/resources/" + fileName + ".m4a";
+        FileInputStream audioFileInputStream = new FileInputStream(filePath);
+        String title = "테스트 파일 제목";
+        String length = "00:48:30";
+
+        MockMultipartFile audioFile = new MockMultipartFile(
+                "file",
+                fileName + "." + fileType,
+                fileType,
+                audioFileInputStream);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(multipart(API_URL)
+                .file(audioFile)
+                .param("title", title)
+                .param("length", length)
+                .header("Authorization", "Bearer " + accessToken)
+                .contentType(MediaType.MULTIPART_FORM_DATA));
+
+        // then
+        resultActions
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data.title").value(title))
+                .andExpect(jsonPath("$.data.cautions").isArray())
                 .andDo(print());
     }
 }
