0a729c2600df511589d7c3449f2978fbe4753278
Yusun Choi <nsuy.ch@gmail.com>
2024-04-03 20:46:09 +0900

    fix: Add file path for prod (#8)

--- src/main/java/com/naeun/naeun_server/global/util/GcsUtil.java
+++ src/main/java/com/naeun/naeun_server/global/util/GcsUtil.java
@@ -12,6 +12,8 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Component
 public class GcsUtil {
@@ -21,9 +23,19 @@ public class GcsUtil {
     @Value("${spring.cloud.gcp.storage.project-id}")
     private String projectId;
 
+    @Value("${spring.profiles.active:}")
+    private String activeProfile;
+
     public String uploadFile(MultipartFile file, String fileName, String ext) throws IOException {
         String keyFileName = "bucket-key.json";
-        InputStream keyFile = ResourceUtils.getURL("classpath:" + keyFileName).openStream();
+
+        InputStream keyFile;
+        if (activeProfile.equals("prod")) {
+            keyFile = Files.newInputStream(Paths.get(keyFileName));
+        } else {
+            keyFile = ResourceUtils.getURL("classpath:" + keyFileName).openStream();
+        }
+
         Storage storage = StorageOptions.newBuilder()
                 .setCredentials(GoogleCredentials.fromStream(keyFile))
                 .build().getService();
